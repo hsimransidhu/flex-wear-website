041acba72f9f89f4eb7938d0c86fa9778c600b58
hsimransidhu <simusidhu2004@gmail.com>
2023-11-14 10:17:57 -0600

    added changes in footer

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,13 @@
+'use strict';
+
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+
 document.getElementById('login').addEventListener('click', openModal);
 function openModal() {
     document.getElementById('loginModal').style.display = 'flex';
@@ -6,5 +16,18 @@ function openModal() {
 // Add this function to close the login modal
 function closeModal() {
     document.getElementById('loginModal').style.display = 'none';
+}
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var toggleIcon = document.querySelector('.toggle-password');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.innerHTML = '&#x1F441'; // Eye icon open
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.innerHTML = '&#x1F441'; // Eye icon closed
+    }
 }
  
