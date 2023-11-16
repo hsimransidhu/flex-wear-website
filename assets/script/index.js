bea4f5439911042cc7618aed66f35ee49ca8ac9b
Gurlinkaur23 <gurlinkaur338@gmail.com>
2023-11-15 20:23:48 -0600

    Create index.js with utility functions

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,33 +1,51 @@
-'use strict';
+"use strict";
 
-const menuIcon = document.querySelector('.menu-icon');
-const navbar = document.querySelector('.navbar');
+// Utility functions
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
 
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function getElement(selector, parent = document) {
+    return parent.getElementById(selector);
+  }
+
+// Selections
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
+const productDetail = getElement('product-1');
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
 });
 
-
-document.getElementById('login').addEventListener('click', openModal);
+document.getElementById("login").addEventListener("click", openModal);
 function openModal() {
-    document.getElementById('loginModal').style.display = 'flex';
+  document.getElementById("loginModal").style.display = "flex";
 }
 
 // Add this function to close the login modal
 function closeModal() {
-    document.getElementById('loginModal').style.display = 'none';
+  document.getElementById("loginModal").style.display = "none";
 }
 
 function togglePasswordVisibility() {
-    var passwordInput = document.getElementById('password');
-    var toggleIcon = document.querySelector('.toggle-password');
+  var passwordInput = document.getElementById("password");
+  var toggleIcon = document.querySelector(".toggle-password");
 
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        toggleIcon.innerHTML = '&#x1F441'; // Eye icon open
-    } else {
-        passwordInput.type = 'password';
-        toggleIcon.innerHTML = '&#x1F441'; // Eye icon closed
-    }
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleIcon.innerHTML = "&#x1F441"; // Eye icon open
+  } else {
+    passwordInput.type = "password";
+    toggleIcon.innerHTML = "&#x1F441"; // Eye icon closed
+  }
 }
- 
+
+// Open detail page
+onEvent('click', productDetail, () => {
+    window.location.href = '';
+})
