411e0f8f6dec4a554d3011610fe85e86239a0e33
hsimransidhu <simusidhu2004@gmail.com>
2023-11-15 23:01:33 -0600

    updated index.js file in branch footer

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,33 +1,33 @@
 'use strict';
 
-const menuIcon = document.querySelector('.menu-icon');
-const navbar = document.querySelector('.navbar');
+let MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
 
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
 
-
-document.getElementById('login').addEventListener('click', openModal);
 function openModal() {
+    if (window.innerWidth <= 600) {
+        menutoggle();
+        // Hide the menu when opening the modal on smaller screens
+        MenuItems.style.maxHeight = "0px";
+    
+        document.getElementById('loginModal').style.display = 'flex';
+    }
     document.getElementById('loginModal').style.display = 'flex';
 }
 
-// Add this function to close the login modal
 function closeModal() {
     document.getElementById('loginModal').style.display = 'none';
 }
 
-function togglePasswordVisibility() {
-    var passwordInput = document.getElementById('password');
-    var toggleIcon = document.querySelector('.toggle-password');
-
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        toggleIcon.innerHTML = '&#x1F441'; // Eye icon open
-    } else {
-        passwordInput.type = 'password';
-        toggleIcon.innerHTML = '&#x1F441'; // Eye icon closed
-    }
+function login() {
+    document.getElementById('loginModal').style.display = 'none';
 }
+
  
